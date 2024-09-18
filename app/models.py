2768f2d045b97556605235cdcc058f0634ff569d
Rodrich94 <rodrigo.chavez@est.fi.uncoma.edu.ar>
2024-09-18 15:01:47 -0300

    Agregamos los modelos que tenia la rama secundaria y corregimos el error de claves foraneas

--- app/models.py
+++ app/models.py
@@ -44,3 +44,69 @@ class Empleado(db.Model):
 
     def __repr__(self):
         return f"<Empleado {self.legajo} - {self.nombre} {self.apellido}>"
+
+
+class Licencia(db.Model):
+    __tablename__ = 'licencias'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_desde = db.Column(db.Date, nullable=False)
+    fecha_hasta = db.Column(db.Date, nullable=False)
+    tipo = db.Column(db.String(20), nullable=False)
+
+    # Foreign Key a la tabla Empleado (se toma licencia)
+    legajo_empleado = db.Column(db.String(20),
+                                db.ForeignKey('empleados.legajo'),
+                                nullable=False)
+    # Foreign Key a la tabla Empleado (autorizante)
+    legajo_autorizante = db.Column(db.String(20),
+                                   db.ForeignKey('empleados.legajo'),
+                                   nullable=False)
+
+    # Relación con Empleado (se toma licencia)
+    empleado = db.relationship('Empleado', foreign_keys=[legajo_empleado])
+
+    # Relación con Empleado (autorizante)
+    autorizante = db.relationship('Empleado', foreign_keys=[legajo_autorizante])
+
+    def __repr__(self):
+        return f"<Licencia {self.fecha_desde} - {self.fecha_hasta}>"
+    
+# Modelo Cupo Mensual
+class CupoMensual(db.Model):
+    __tablename__ = 'cupos_mensuales'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_ini = db.Column(db.Date, nullable=False)
+    fecha_fin = db.Column(db.Date, nullable=False)
+    total = db.Column(db.Double, nullable=False)
+    remanente = db.Column(db.Double, nullable=False)
+
+    # Foreign Key a la tabla Servicio
+    id_servicio = db.Column(db.Integer,
+                            db.ForeignKey('servicios.id'),
+                            nullable=False)
+    # Foreign Key a la tabla Empleado (autorizante)
+    legajo_autorizante = db.Column(db.String(20),
+                                   db.ForeignKey('empleados.legajo'),
+                                   nullable=False)
+
+    def __repr__(self):
+        return f"<CupoMensual {self.fecha_desde} - {self.fecha_hasta}>"
+
+# Modelo DiagramaMensual
+class DiagramaMensual(db.Model):
+    __tablename__ = 'diagramas_mensuales'
+
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_ini = db.Column(db.Date, nullable=False)
+    fecha_fin = db.Column(db.Date, nullable=False)
+    estado = db.Column(db.String(20), nullable=False)
+
+    # Foreign Key a la tabla Servicio
+    id_servicio = db.Column(db.Integer,
+                            db.ForeignKey('servicios.id'),
+                            nullable=False)
+
+    def __repr__(self):
+        return f"<DiagramaMensual {self.fecha_ini} - {self.fecha_fin}>"
