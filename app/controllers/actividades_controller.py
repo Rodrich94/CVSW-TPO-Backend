ddd0e9a65bf29b1b85de413cce08723661a1ba7f
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2024-10-29 16:00:35 -0300

    Agrega controller de actividades

---
+++ app/controllers/actividades_controller.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+from ..utils.utils import verificar_empleado, validar_rango_fechas, obtener_actividades_empleado
+
+
+def obtener_actividades(legajo_empleado):
+    """
+    Consulta de actividades por empleado.
+    """
+    data = request.args
+    fecha_desde = data.get('fecha_desde')
+    fecha_hasta = data.get('fecha_hasta')
+
+    # Verificar empleado
+    empleado_valido, error = verificar_empleado(legajo_empleado)
+    if not empleado_valido:
+        return jsonify({'error': error}), 400
+
+    # Validar fechas desde y hasta
+    fechas_validas, error = validar_rango_fechas(fecha_desde, fecha_hasta)
+    if not fechas_validas:
+        return jsonify({'error': error}), 400
+
+    # TODO: obtener actividades
+    actividades = obtener_actividades_empleado(legajo_empleado, fecha_desde, fecha_hasta)
+
+    return jsonify(actividades), 200
