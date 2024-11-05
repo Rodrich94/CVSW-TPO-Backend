d260c4abd47e607609637ee501f9600a8c1e8cf7
Nicolás Comeron <nicolas.comeron@est.fi.uncoma.edu.ar>
2024-11-04 23:22:08 -0300

    Agregar rutas Guardia

--- app/routes/routerGuardia.py
+++ app/routes/routerGuardia.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.guardia_controller import obtener_guardias, modificar_guardia, crear_guardias
+from app.controllers.guardia_controller import obtener_guardias, crear_guardias, obtener_guardias_por_servicio_tipo, modificar_empleado_guardia
 
 router_guardia = Blueprint('guardias', __name__)
 
@@ -7,10 +7,14 @@ router_guardia = Blueprint('guardias', __name__)
 def listar_guardias():
     return obtener_guardias()
 
-@router_guardia.route('/guardia', methods=['PUT'])
-def cambio_empleado_en_guardia():
-    return modificar_guardia()
-
 @router_guardia.route('/guardia/empleado', methods=['POST'])
 def agregar_guardias():
     return crear_guardias()
+
+@router_guardia.route('/guardias/servicio-tipo', methods=['GET'])
+def listar_guardias_por_servicio_tipo():
+    return obtener_guardias_por_servicio_tipo()
+
+@router_guardia.route('/guardia/cambiar-empleado/<int:id_guardia>', methods=['PUT'])
+def cambio_empleado_en_guardia(id_guardia):
+    return modificar_empleado_guardia(id_guardia)
