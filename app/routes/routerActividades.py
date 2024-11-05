3da5cac8586de6fb493a476ef4748b1a46b76f05
Nicolás Comeron <nicolas.comeron@est.fi.uncoma.edu.ar>
2024-11-04 23:26:38 -0300

    Agregar rutas Actividades

--- app/routes/routerActividades.py
+++ app/routes/routerActividades.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.actividades_controller import obtener_actividades
+from app.controllers.actividades_controller import obtener_actividades, obtener_resumen_actividades_por_servicio
 
 
 router_actividades = Blueprint('actividades', __name__)
@@ -8,3 +8,7 @@ router_actividades = Blueprint('actividades', __name__)
 @router_actividades.route('/actividades/empleado/<string:legajo_empleado>', methods=['GET'])
 def get_actividades(legajo_empleado):
     return obtener_actividades(legajo_empleado)
+
+@router_actividades.route('/actividades/servicio/<int:id_servicio>', methods=['GET'])
+def get_resumen_actividades_por_servicio(id_servicio):
+    return obtener_resumen_actividades_por_servicio(id_servicio)
