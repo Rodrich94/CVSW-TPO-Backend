eddf2618128bef3234bb8d2d25d709055dfe045f
Nicolás Comeron <nicolas.comeron@est.fi.uncoma.edu.ar>
2024-11-04 23:28:34 -0300

    Agregar transacción Resumen de Actividades por Servicio

--- app/controllers/actividades_controller.py
+++ app/controllers/actividades_controller.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from ..utils.utils import verificar_empleado, validar_rango_fechas, obtener_actividades_empleado
+from ..utils.utils import verificar_empleado, validar_rango_fechas, obtener_actividades_empleado, verificar_servicio, obtener_resumen_actividades_empleado
 
 
 def obtener_actividades(legajo_empleado):
@@ -24,3 +24,26 @@ def obtener_actividades(legajo_empleado):
     actividades = obtener_actividades_empleado(legajo_empleado, fecha_desde, fecha_hasta)
 
     return jsonify(actividades), 200
+
+
+def obtener_resumen_actividades_por_servicio(id_servicio):
+    """
+    Resumen de actividades por servicio.
+    """
+    data = request.get_json()
+    fecha_desde = data.get('fecha_desde')
+    fecha_hasta = data.get('fecha_hasta')
+
+    # Verificar servicio
+    validacion_servicio, mensaje = verificar_servicio(id_servicio)
+    if not validacion_servicio:
+        return jsonify({'error': mensaje}), 404
+    
+    validacion_fechas, mensaje = validar_rango_fechas(fecha_desde, fecha_hasta)
+    if not validacion_fechas:
+        return jsonify({'error': mensaje}), 400
+
+    # Obtener el resumen de las actividades de un servicio de cada empleado
+    resumen_actividades_por_servicio = obtener_resumen_actividades_empleado(id_servicio, fecha_desde, fecha_hasta)
+
+    return jsonify(resumen_actividades_por_servicio), 200
